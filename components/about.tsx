36b24c9dd29563df75b1e2c5e217e4689b17473e
kushtrim94 <kushtrimmarke12@gmail.com>
2024-10-08 11:32:30 +0200

    changes

--- components/about.tsx
+++ components/about.tsx
@@ -1,37 +1,37 @@
-'use client';
+"use client";
 
-import React from 'react';
-import SectionHeading from './section-heading';
-import { motion } from 'framer-motion';
-import { useSectionInView } from '@/lib/hooks';
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
-  const { ref } = useSectionInView('About');
+  const { ref } = useSectionInView("About");
 
   return (
     <motion.section
       ref={ref}
-      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id='about'
+      id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className='mb-3'>
-        I'm Kushtrim Marke, a React developer with 2 years of experience.{' '}
-        <span className='font-medium'>I'm passionate</span> about creating
+      <p className="mb-3">
+        I'm Kushtrim Marke, a React developer with 3 years of experience.{" "}
+        <span className="font-medium">I'm passionate</span> about creating
         responsive web solutions and optimizing user experiences.I'm always
-        looking to learn new technologies and grow as a{' '}
-        <span className='font-medium'>developer</span>.{' '}
-        <span className='italic'>My favorite part of programming</span> is the
-        problem-solving aspect. I <span className='underline'>love</span> the
+        looking to learn new technologies and grow as a{" "}
+        <span className="font-medium">developer</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is <span className='font-medium'>React, Next.js</span>. I am also
+        is <span className="font-medium">React, Next.js</span>. I am also
         familiar with
-        <span className='font-medium'>TypeScript and Prisma. </span> I am
-        currently looking for a{' '}
-        <span className='font-medium'>full-time position</span> as a software
+        <span className="font-medium">TypeScript and Prisma. </span> I am
+        currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
 
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,76 +1,74 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
-import { HiDownload } from 'react-icons/hi';
-import { FaGithubSquare } from 'react-icons/fa';
-import { useSectionInView } from '@/lib/hooks';
-import { useActiveSectionContext } from '@/context/active-section-context';
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
-  const { ref } = useSectionInView('Home', 0.5);
+  const { ref } = useSectionInView("Home", 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
       ref={ref}
-      id='home'
-      className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <div className='flex items-center justify-center'>
-        <div className='relative'>
+      <div className="flex items-center justify-center">
+        <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-              type: 'tween',
+              type: "tween",
               duration: 0.2,
             }}
           >
             <Image
-              src='/profile.jpeg'
-              alt='Kushtrim portrait'
-              width='200'
-              height='200'
-              quality='95'
+              src="/profile.jpeg"
+              alt="Kushtrim portrait"
+              width="200"
+              height="200"
+              quality="95"
               priority={true}
-              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
           <motion.span
-            className='absolute bottom-0 right-0 text-4xl'
+            className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-              type: 'spring',
+              type: "spring",
               stiffness: 125,
               delay: 0.1,
               duration: 0.7,
             }}
-          >
-            
-          </motion.span>
+          ></motion.span>
         </div>
       </div>
 
       <motion.h1
-        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className='font-bold'>Hello, I'm Kushtrim</span> I'm a{' '}
-        <span className='font-bold'>frontend developer</span> with{' '}
-        <span className='font-bold'>2 years</span> of experience. I enjoy
-        building <span>sites & apps</span>. My focus is{' '}
-        <span className='underline'>React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Kushtrim</span> I'm a{" "}
+        <span className="font-bold">frontend developer</span> with{" "}
+        <span className="font-bold">3 years</span> of experience. I enjoy
+        building <span>sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
-        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -78,29 +76,29 @@ export default function Intro() {
         }}
       >
         <Link
-          href='#contact'
-          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
-            setActiveSection('Contact');
+            setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
           }}
         >
-          Contact me here{' '}
-          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        
+
         <a
-          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://www.linkedin.com/in/kushtrim-marke/'
-          target='_blank'
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/kushtrim-marke/"
+          target="_blank"
         >
           <BsLinkedin />
         </a>
 
         <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-          href='https://github.com'
-          target='_blank'
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com"
+          target="_blank"
         >
           <FaGithubSquare />
         </a>
