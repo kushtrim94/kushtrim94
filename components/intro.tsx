0fcb6bdd41edfe658771611f592c97f0bdaf7e5e
kushtrim94 <kushtrimmarke12@gmail.com>
2025-04-01 11:48:15 +0200

    latest update

--- components/intro.tsx
+++ components/intro.tsx
@@ -60,11 +60,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Kushtrim</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Kushtrim</span>. I'm a{" "}
         <span className="font-bold">frontend developer</span> with{" "}
-        <span className="font-bold">3+ years</span> of experience. I enjoy
-        building <span>sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">3+ years</span> of experience in{" "}
+        <span className="underline">React and Next.js</span>, creating dynamic
+        websites and applications.
       </motion.h1>
 
       <motion.div
